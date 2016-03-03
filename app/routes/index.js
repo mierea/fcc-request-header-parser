66fc8c911cfbc152040a0fece55c9b6282660c19
mhm <mierea.ciprian@gmail.com>
2016-03-03 23:20:29 +0000

    index.js done

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,56 +2,40 @@
 
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
-
+var moment = require('moment');
 module.exports = function (app, passport) {
 
 	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
+		//if (req.isAuthenticated()) {
 			return next();
-		} else {
-			res.redirect('/login');
-		}
+		//} else {
+	//		res.redirect('/login');
+		//}
 	}
 
 	var clickHandler = new ClickHandler();
+
+
 
 	app.route('/')
 		.get(isLoggedIn, function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+       app.get('/:query', function(req, res) {
+        var date = req.params.query;
+		res.setHeader('Content-Type', 'application/json');
+		if(!isNaN(parseInt(date))){
+			 res.send({"unix": parseInt(date), "natural": moment.unix(date).format("MMMM D, YYYY")});
+		} else if (moment(date, "MMMM D, YYYY").isValid()){
+			 res.send({"unix":parseInt(moment.unix(moment(date, "MMMM D, YYYY").unix()).format("X")), "natural": date});
+		}
+		else {
+			res.send({"unix":null,"natural":null});
+		}
 		});
+		
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
